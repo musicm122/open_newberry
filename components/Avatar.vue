6f3aee1b706bf36c34113a7670cb326cb2ae9395
musicm122 <terrance@hackerferret.com>
2021-01-09 11:49:03 -0500

    feat: add dumb components for common elements

---
+++ components/Avatar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mb-2">
+    <b-avatar :src="imageSrc"></b-avatar>
+    <b-avatar src="https://placekitten.com/300/300" size="6rem"></b-avatar>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    imageSrc: {
+      type: String,
+      default: '',
+    },
+    userName: {
+      type: String,
+      default: 'Anon',
+    },
+    profileLink: {
+      type: String,
+      default: 'https://www.google.com',
+    },
+    role: {
+      type: String,
+      default: 'User',
+    },
+    size: {
+      type: String,
+      default: 'SM',
+    },
+  },
+}
+</script>
--- components/Nav.vue
+++ components/Nav.vue
@@ -7,6 +7,7 @@
     <b-collapse id="nav-collapse" is-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+        <b-nav-item href="/suggestions">Suggestions</b-nav-item>
         <b-nav-item href="/reports">Reports</b-nav-item>
         <b-nav-item href="/about">About</b-nav-item>
       </b-navbar-nav>
